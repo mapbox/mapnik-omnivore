257b91d0d037492ee99d9f11603d987689576383
Dane Springmeyer <dane@mapbox.com>
2015-11-25 16:32:18 -0500

    test for invalid usage of digest script

--- test/digest.test.js
+++ test/digest.test.js
@@ -5,6 +5,20 @@ var testData = path.join(path.dirname(require.resolve('mapnik-test-data')), 'dat
 var fixture = path.join(testData, 'geojson', 'DC_polygon.geo.json');
 var spawn = require('child_process').spawn;
 
+test('[bin/digest] errors if not passed valid path', function(assert) {
+  var args = [digest];
+
+  spawn(process.execPath, args)
+    .on('error', function(err) {
+      assert.ok(err, 'should error');
+    })
+    .on('close', function(code) {
+      assert.equal(code, 1, 'exit 1');
+      assert.end();
+    })
+    .stderr.pipe(process.stdout);
+});
+
 test('[bin/digest] runs on an absolute file path', function(assert) {
   var args = [digest, fixture];
 
