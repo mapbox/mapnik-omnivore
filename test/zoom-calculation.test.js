3aa7832efb4c12cbfcee94ae28d2f6ff5a0b05ab
mapsam <matthews.sam@gmail.com>
2016-11-04 09:34:55 -0700

    set smallest max zoom for point files

--- test/zoom-calculation.test.js
+++ test/zoom-calculation.test.js
@@ -3,98 +3,102 @@ var GeoJSON = require('../lib/geojson.js');
 var mapnik = require('mapnik');
 mapnik.register_default_input_plugins();
 
-tape('[zoom calculation] poi-calgary.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/poi-calgary.geo.json';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+tape('[zoom calculation] points', function(assert) {
+  tape('[points] poi-calgary.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/poi-calgary.geo.json';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 14);
+      assert.end();
+    });
   });
-});
 
-tape('[zoom calculation] single-point.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/single-point.geojson';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+  tape('[points] single-point.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/single-point.geojson';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 11);
+      assert.end();
+    });
   });
-});
 
-tape('[zoom calculation] two-points-close.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/two-points-close.geojson';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+  tape('[points] two-points-close.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/two-points-close.geojson';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 12);
+      assert.end();
+    });
   });
-});
 
-tape('[zoom calculation] two-points-far.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/two-points-far.geojson';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+  tape('[points] two-points-far.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/two-points-far.geojson';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 11);
+      assert.end();
+    });
   });
-});
 
-tape('[zoom calculation] two-points-medium.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/two-points-medium.geojson';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+  tape('[points] two-points-medium.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/two-points-medium.geojson';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 11);
+      assert.end();
+    });
   });
-});
 
-tape('[zoom calculation] us-capitals.geojson', function(assert) {
-  var file = 'test/fixtures/zoom-ratio/us-capitals.geojson';
-  var source = new GeoJSON(file);
-  source.getZooms(function(err, minzoom, maxzoom) {
-    if (err) {
-      assert.ifError(err, 'should not error');
-      return assert.end();
-    }
+  tape('[points] us-capitals.geojson', function(assert) {
+    var file = 'test/fixtures/zoom-ratio/us-capitals.geojson';
+    var source = new GeoJSON(file);
+    source.getZooms(function(err, minzoom, maxzoom) {
+      if (err) {
+        assert.ifError(err, 'should not error');
+        return assert.end();
+      }
 
-    assert.ok(err === null);
-    console.log(file);
-    console.log(minzoom, maxzoom);
-    assert.end();
+      assert.ok(err === null);
+      assert.equal(minzoom, 0);
+      assert.equal(maxzoom, 11);
+      assert.end();
+    });
   });
+
+  assert.end();
 });
